6af520dfcefb77d68e5f171a1428d2f49b482f66
MathiasMauge7 <mathias.mauge@gmail.com>
2023-04-06 15:10:14 +0200

    templating exo

--- exo/Jour4/templating/index.js
+++ exo/Jour4/templating/index.js
@@ -1,15 +1,16 @@
 const pug = require("pug");
+let user = { isAdmin: true };
 
 //----------compileFile pour un fichier pug-----------//
-// const compileTemplate = pug.compileFile("template.pug");
+const compileTemplate = pug.compileFile("template.pug");
 
-// const result = compileTemplate({ isAdmin: false });
+const result = compileTemplate({ user });
 
-// console.log(result);
+console.log(result);
 
 //---------------- Méthode => Render ----------------//
 
-pug.renderFile("template.pug", { isAdmin: false }, (err, data) => {
+pug.renderFile("template.pug", { user }, (err, data) => {
   if (err) throw err;
   console.log(data);
 });
